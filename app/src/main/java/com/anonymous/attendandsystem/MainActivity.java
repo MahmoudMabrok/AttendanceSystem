b24d7cc35fa01bdc661dc169db80dff7e8e14bdc
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2018-03-04 11:41:31 +0200

    add shared prefenece & solve NumberMismath exception

--- app/src/main/java/com/anonymous/attendandsystem/MainActivity.java
+++ app/src/main/java/com/anonymous/attendandsystem/MainActivity.java
@@ -3,13 +3,14 @@ package com.anonymous.attendandsystem;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.Preference;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.InputMismatchException;
 
 public class MainActivity extends Activity {
 
@@ -23,29 +24,27 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-/*
-
-        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(getString(R.string.saved_high_score_key), newHighScore);
-        editor.commit();*/
-
-
-
-        btn_enter = (Button)findViewById(R.id.btnEnter);
         tv_hint = (TextView)findViewById(R.id.labelText);
         input = (EditText)findViewById(R.id.input);
 
-        btn_enter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              String in = input.getText().toString() ;
 
-            }
-        });
+    }
+
+    public void saveId (View view ){
+        SharedPreferences shared = getSharedPreferences("id" , Context.MODE_PRIVATE) ;
+        SharedPreferences.Editor editor = shared.edit() ;
+        try {
+            editor.putInt("id", Integer.parseInt(input.getText().toString()));
+            editor.apply();
+        }catch (NumberFormatException s){
+            Toast.makeText(this , "Error " ,Toast.LENGTH_LONG ).show();
+        }
+
+    }
+    public void getId (View view){
+        SharedPreferences shared = getSharedPreferences("id" , Context.MODE_PRIVATE) ;
+        id = shared.getInt("id" , 0) ;
+        tv_hint.setText(""+id);
 
     }
 }
