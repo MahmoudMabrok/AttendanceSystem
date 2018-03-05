29b2f6d204185ee21ce4c9a99a5dee0f6131aedc
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2018-03-05 22:54:58 +0200

    add login form

---
+++ app/src/main/java/com/anonymous/attendandsystem/LogIn.java
@@ -0,0 +1,69 @@
+package com.anonymous.attendandsystem;
+
+/**
+ * Created by motamed on 3/5/2018.
+ */
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.id.message;
+
+
+public class LogIn extends Activity {
+
+    private  int id  = -1 ;
+
+    EditText input ;
+    EditText userName ;
+    ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        input = (EditText)findViewById(R.id.password) ;
+        userName = (EditText)findViewById(R.id.userName) ;
+        getData();
+        setContentView(R.layout.log_in);
+        if (id > -1)
+           startNewActivity();
+
+    }
+
+    public void saveData (View view ){
+        SharedPreferences shared = getSharedPreferences("UserData" , Context.MODE_PRIVATE) ;
+        SharedPreferences.Editor editor = shared.edit() ;
+        try {
+            editor.putString("name" , userName.getText().toString()) ;
+            editor.putInt("id", Integer.parseInt(input.getText().toString()));
+            editor.apply();
+        }catch (NumberFormatException s){
+            Toast.makeText(this , "Error " ,Toast.LENGTH_LONG ).show();
+        }
+
+    }
+    public void getData (){
+       try {
+           SharedPreferences shared = getSharedPreferences("UserData", Context.MODE_PRIVATE);
+           id = shared.getInt("id", 0);
+
+       }catch (Exception e){
+           id = -1 ;
+       }
+    }
+    public void startNewActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.putExtra("id", id);
+        startActivity(intent);
+
+    }
+}
--- app/src/main/java/com/anonymous/attendandsystem/MainActivity.java
+++ app/src/main/java/com/anonymous/attendandsystem/MainActivity.java
@@ -2,6 +2,7 @@ package com.anonymous.attendandsystem;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -10,14 +11,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.InputMismatchException;
 
 public class MainActivity extends Activity {
 
-    private  int id =0 ;
-    Button btn_enter ;
     TextView tv_hint ;
-    EditText input ;
+
    ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +23,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         tv_hint = (TextView)findViewById(R.id.labelText);
-        input = (EditText)findViewById(R.id.input);
+        Intent intent = getIntent();
+        tv_hint.setText(intent.getIntExtra("id" , -1 ));
 
 
-    }
-
-    public void saveId (View view ){
-        SharedPreferences shared = getSharedPreferences("id" , Context.MODE_PRIVATE) ;
-        SharedPreferences.Editor editor = shared.edit() ;
-        try {
-            editor.putInt("id", Integer.parseInt(input.getText().toString()));
-            editor.apply();
-        }catch (NumberFormatException s){
-            Toast.makeText(this , "Error " ,Toast.LENGTH_LONG ).show();
-        }
 
     }
-    public void getId (View view){
-        SharedPreferences shared = getSharedPreferences("id" , Context.MODE_PRIVATE) ;
-        id = shared.getInt("id" , 0) ;
-        tv_hint.setText(""+id);
 
-    }
 }
