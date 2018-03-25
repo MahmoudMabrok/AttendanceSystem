fd86fcbd2ad23e16facf561c32785d47aaa99a5c
xMansour <memobooos@yahoo.com>
2018-03-25 23:52:17 +0200

    Connect to the nearest open Wifi hotspot added

--- app/src/main/java/com/anonymous/attendandsystem/Student.java
+++ app/src/main/java/com/anonymous/attendandsystem/Student.java
@@ -1,21 +1,42 @@
 package com.anonymous.attendandsystem;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 /**
  * Created by motamed on 3/6/2018.
  */
 
 public class Student  extends Activity  {
 
-
+    WifiManager wifi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student);
+
+        Button btnJoin = (Button)findViewById(R.id.btnJoin);
+
+        wifi = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (wifi.isWifiEnabled() == false) {
+                    Toast.makeText(getApplicationContext(), "Connecting...", Toast.LENGTH_LONG).show();
+                    wifi.setWifiEnabled(true);
+                }
+            }
+            });
+
+
+
 
 
         TextView   tv_hint = (TextView)findViewById(R.id.labelText);
@@ -24,5 +45,8 @@ public class Student  extends Activity  {
         id = intent.getIntExtra("id" , -1 ) ;
         tv_hint.setText("Welcome Student No."+id );
 
+
     }
+
+
 }
