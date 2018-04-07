b1ab8762d6fea753b2fb0583c5fe18f397b25192
xMansour <memobooos@yahoo.com>
2018-04-08 00:08:11 +0200

    System Designed

--- app/src/main/java/com/anonymous/attendandsystem/MainActivity.java
+++ app/src/main/java/com/anonymous/attendandsystem/MainActivity.java
@@ -1,15 +1,9 @@
 package com.anonymous.attendandsystem;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
--- app/src/main/java/com/anonymous/attendandsystem/Student.java
+++ app/src/main/java/com/anonymous/attendandsystem/Student.java
@@ -10,34 +10,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
-/**
- * Created by motamed on 3/6/2018.
- */
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class Student  extends Activity  {
 
-    WifiManager wifi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student);
 
-        Button btnJoin = (Button)findViewById(R.id.btnJoin);
-
-        wifi = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
-        btnJoin.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (wifi.isWifiEnabled() == false) {
-                    Toast.makeText(getApplicationContext(), "Connecting...", Toast.LENGTH_LONG).show();
-                    wifi.setWifiEnabled(true);
-                }
-            }
-            });
-
-
-
-
+        Button btnScan = (Button)findViewById(R.id.btnScan);
+        final Activity ACTIVITY = this;
 
         TextView   tv_hint = (TextView)findViewById(R.id.labelText);
         Intent intent = getIntent();
@@ -45,7 +30,35 @@ public class Student  extends Activity  {
         id = intent.getIntExtra("id" , -1 ) ;
         tv_hint.setText("Welcome Student No."+id );
 
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(ACTIVITY);
+                intentIntegrator.setDesiredBarcodeFormats(intentIntegrator.QR_CODE_TYPES);
+                intentIntegrator.setPrompt("SCAN");
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.setBarcodeImageEnabled(false);
+                intentIntegrator.initiateScan();
 
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(intentResult != null){
+            //You should send his id to the server here instead of that toast
+            if(intentResult.getContents()==null){
+                Toast.makeText(this,"You cancelled the scanning",Toast.LENGTH_LONG).show();
+
+            }
+            else{
+                Toast.makeText(this, intentResult.getContents(), Toast.LENGTH_LONG).show();
+            }
+
+        }
+        else{
+            super.onActivityResult(requestCode,resultCode,data);
+        }
     }
 
 
